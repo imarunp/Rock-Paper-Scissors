0936d9a6f380696d34e2910c1e01a9994e1f5a51
Arun <arcanearun@gmail.com>
2023-10-04 09:30:20 +0545

    css changes

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const paperBtn = document.getElementById("btn-paper");
-const scissorsBtn = document.getElementById("btn-scissors");
-const rockBtn = document.getElementById("btn-rock");
-const output = document.getElementById("output");
-const compOp = document.getElementById("computerScore");
-const tieOp = document.getElementById("tieScore");
-const playerOp = document.getElementById("playerScore");
-const selection = document.getElementById("selection");
-const winner = document.getElementById("winner");
+const paperBtn = document.getElementById('btn-paper');
+const scissorsBtn = document.getElementById('btn-scissors');
+const rockBtn = document.getElementById('btn-rock');
+const output = document.getElementById('output');
+const compOp = document.getElementById('computerScore');
+const tieOp = document.getElementById('tieScore');
+const playerOp = document.getElementById('playerScore');
+const selection = document.getElementById('selection');
+const winner = document.getElementById('winner');
+
 function getComputerChoice() {
-  const arr = ["Rock", "Paper", "Scissors"];
+  const arr = ['Rock', 'Paper', 'Scissors'];
   const compChoice = arr[Math.floor(Math.random() * 3)];
   return compChoice;
 }
-paperBtn.addEventListener("click", () => {
-  playerSelection = "paper";
+paperBtn.addEventListener('click', () => {
+  playerSelection = 'paper';
+
   computerSelection = getComputerChoice().toLowerCase();
   selection.textContent = `You chose ${playerSelection} and computer chose ${computerSelection}`;
   output.textContent = playRound(playerSelection, computerSelection);
@@ -26,8 +28,8 @@ paperBtn.addEventListener("click", () => {
     winner.textContent = `The winner is Player. Congratulations`;
   }
 });
-scissorsBtn.addEventListener("click", () => {
-  playerSelection = "scissors";
+scissorsBtn.addEventListener('click', () => {
+  playerSelection = 'scissors';
   computerSelection = getComputerChoice().toLowerCase();
   selection.textContent = `You chose ${playerSelection} and computer chose ${computerSelection}`;
   output.textContent = playRound(playerSelection, computerSelection);
@@ -40,8 +42,8 @@ scissorsBtn.addEventListener("click", () => {
     winner.textContent = `You Won. Congratulations`;
   }
 });
-rockBtn.addEventListener("click", () => {
-  playerSelection = "rock";
+rockBtn.addEventListener('click', () => {
+  playerSelection = 'rock';
   computerSelection = getComputerChoice().toLowerCase();
   selection.textContent = `You chose ${playerSelection} and computer chose ${computerSelection}`;
   output.textContent = playRound(playerSelection, computerSelection);
@@ -60,56 +62,38 @@ let computerScore = 0;
 let tieScore = 0;
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === "rock" && computerSelection === "paper") {
+  if (playerSelection === 'rock' && computerSelection === 'paper') {
     computerScore++;
     return loss;
-  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+  } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
     playerScore++;
     return win;
-  } else if (playerSelection == "rock" && computerSelection == "rock") {
+  } else if (playerSelection == 'rock' && computerSelection == 'rock') {
     tieScore++;
     return tie;
   }
-  if (playerSelection === "paper" && computerSelection === "scissors") {
+  if (playerSelection === 'paper' && computerSelection === 'scissors') {
     computerScore++;
     return loss;
-  } else if (playerSelection == "paper" && computerSelection == "rock") {
+  } else if (playerSelection == 'paper' && computerSelection == 'rock') {
     playerScore++;
     return win;
-  } else if (playerSelection == "paper" && computerSelection == "paper") {
+  } else if (playerSelection == 'paper' && computerSelection == 'paper') {
     tieScore++;
     return tie;
   }
 
-  if (playerSelection === "scissors" && computerSelection === "rock") {
+  if (playerSelection === 'scissors' && computerSelection === 'rock') {
     computerScore++;
     return loss;
-  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+  } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
     playerScore++;
     return win;
-  } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+  } else if (playerSelection == 'scissors' && computerSelection == 'scissors') {
     tieScore++;
     return tie;
   }
 }
-const win = "You win this round! \n ----";
-const loss = "You lose this round! \n ----";
-const tie = "This round is a tie! \n ----";
-
-// function game() {
-//   for (let i = 0; i < 5; i++) {
-//     // playerSelection = prompt("Enter paper, scissors or rock").toLowerCase();
-//     computerSelection = getComputerChoice().toLowerCase();
-//     const currentRound = playRound(playerSelection, computerSelection);
-//     console.log(currentRound);
-//   }
-//   if (playerScore > computerScore) {
-//     return "You win this game. \n ----";
-//   }
-//   if (playerScore === computerScore) {
-//     return "This game is a tie. \n ----";
-//   } else {
-//     return "You lose this game. \n ----";
-//   }
-// }
-// console.log(game());
+const win = 'You win this round! \n ----';
+const loss = 'You lose this round! \n ----';
+const tie = 'This round is a tie! \n ----';
